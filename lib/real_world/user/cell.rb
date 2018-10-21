9149977f049face3dcce959fd195910454527bc3
larribas <lorenzo.s.arribas@gmail.com>
2018-10-21 11:39:55 +0200

    Write and test ReadUserById query

--- lib/real_world/user/cell.rb
+++ lib/real_world/user/cell.rb
@@ -6,6 +6,7 @@ require 'real_world/user/materializer'
 require 'real_world/user/commands/create_user'
 require 'real_world/user/commands/update_user'
 require 'real_world/user/queries/read_user_by_email'
+require 'real_world/user/queries/read_user_by_id'
 
 module RealWorld
   module User
@@ -13,16 +14,18 @@ module RealWorld
       include Colmena::Cell
 
       register_port :repository
+      register_port :event_publisher
 
       TRANSACTION = Colmena::Transactions::Materialize[
-        materializer: Materializer,
-        topic: :user_events,
+        event_materializer: Materializer,
+        event_stream: :user_events,
       ]
 
       register_command TRANSACTION[Commands::CreateUser]
       register_command TRANSACTION[Commands::UpdateUser]
 
       register_query Queries::ReadUserByEmail
+      register_query Queries::ReadUserById
     end
   end
 end
