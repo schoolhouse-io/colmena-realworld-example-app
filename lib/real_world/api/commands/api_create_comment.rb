cd7c34b8e3ef34e84c749de2095bc382cb81c927
larribas <lorenzo.s.arribas@gmail.com>
2018-11-03 20:24:13 +0100

    Expose comment behavior through HTTP API and add comment cell to development stack

---
+++ lib/real_world/api/commands/api_create_comment.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+require 'colmena/command'
+
+module RealWorld
+  module Api
+    module Commands
+      class ApiCreateComment < Colmena::Command
+        def call(auth_token:, article_slug:, body:)
+          token, error = port(:tokens).decode_auth(auth_token)
+          return error_response(:forbidden, reason: error) if error
+
+          read_article = port(:router).query(:read_article_by_slug).call(
+            slug: article_slug,
+          )
+
+          capture_errors(read_article) do
+            article = read_article.fetch(:data)
+
+            create_comment = port(:router).command(:create_comment).call(
+              body: body,
+              article_id: article.fetch(:id),
+              author_id: token.fetch(:user_id),
+            )
+
+            capture_errors(create_comment) do
+              comment = create_comment.fetch(:data)
+
+              author = port(:router).query(:read_user_by_id).call(
+                id: comment.fetch(:author_id),
+              ).fetch(:data)
+
+              response(
+                comment: comment.merge(
+                  author: port(:router).query(:api_get_profile).call(
+                    auth_token: auth_token,
+                    username: author.fetch(:username),
+                  ).fetch(:data),
+                ),
+              )
+            end
+          end
+        end
+      end
+    end
+  end
+end
---
+++ lib/real_world/api/commands/api_delete_comment.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require 'colmena/command'
+
+module RealWorld
+  module Api
+    module Commands
+      class ApiDeleteComment < Colmena::Command
+        def call(auth_token:, id:)
+          token, error = port(:tokens).decode_auth(auth_token)
+          return error_response(:forbidden, reason: error) if error
+
+          read_comment = port(:router).query(:read_comment_by_id).call(
+            id: id,
+          )
+
+          capture_errors(read_comment) do
+            comment = read_comment.fetch(:data)
+
+            return error_response(:forbidden) unless comment.fetch(:author_id) == token.fetch(:user_id)
+
+            delete_comment = port(:router).command(:delete_comment).call(
+              id: id,
+            )
+
+            capture_errors(delete_comment) do
+              response(true)
+            end
+          end
+        end
+      end
+    end
+  end
+end
