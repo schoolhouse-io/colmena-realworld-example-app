489d35e358f57bfb74cfdd1ac54bdf6c36f35085
Héctor Ramón Jiménez <hector0193@gmail.com>
2018-11-03 03:32:35 +0100

    Test article commands

---
+++ lib/real_world/article/commands/unfavorite_article_spec.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+require 'real_world/article/ports/spec_shared_context'
+require 'real_world/article/spec_factory_shared_context'
+require 'real_world/article/commands/unfavorite_article'
+require 'securerandom'
+
+describe RealWorld::Article::Commands::UnfavoriteArticle do
+  include_context 'article ports'
+  include_context 'article factory'
+
+  let(:command) { described_class.new(ports) }
+  let(:user_id) { SecureRandom.uuid }
+
+  subject do
+    command.call(
+      id: id,
+      user_id: user_id,
+    )
+  end
+
+  context 'when the article does not exist' do
+    it { is_expected.to fail_with_errors(:article_does_not_exist) }
+  end
+
+  context 'when the article exists' do
+    before { repository.create(some_article) }
+
+    context 'when the user has not favorited the article yet' do
+      it { is_expected.to fail_with_errors(:article_not_favorited) }
+    end
+
+    context 'when the user has favorited the article already' do
+      before { repository.favorite(some_article.merge(favorites_count: 1), user_id) }
+
+      it {
+        is_expected.to(
+          succeed(
+            data: include(favorites_count: 0),
+            events: [:article_unfavorited],
+          ),
+        )
+      }
+    end
+  end
+end
